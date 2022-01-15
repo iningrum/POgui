9b335919dd53ffcf94f132d7518455f66b0831cb
inigrum <dorin_ingrum@outlook.com>
2022-01-15 14:20:15 +0100

    short program for finding out screen dimensions

---
+++ getScreenDimensions.cpp
@@ -0,0 +1,17 @@
+// DO NOT LINK
+// Works only for linux that runs xorg
+#include <string>
+#include <fstream>
+#include <X11/Xlib.h>
+#include <iostream>
+
+int main()
+{
+    Display *d = XOpenDisplay(NULL);
+    Screen *s = DefaultScreenOfDisplay(d);
+    std::ofstream dimensions("./lib/dimensions.h",std::ofstream::trunc);
+    //dimensions.open("gg.txt");
+    //dimensions.open("../lib/dimensions.h");
+    dimensions << "#define MAX_H " + std::to_string(s->height) << "\n#define MAX_W " + std::to_string(s->width) <<std::endl;
+    dimensions.close();
+}
