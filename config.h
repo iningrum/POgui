e9090ec39f64d5ec5d825ab010758a657ac80071
inigrum <dorin_ingrum@outlook.com>
2022-01-25 13:50:54 +0100

    Final product i guess...

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <string>
 using str = const std::string;
-str PO_UTILITIES = "/home/pontiff/CppProjects/PO22/ty/POgui/PO22utils/main.py";
-str CSV_READING_FOLDER = "/home/pontiff/CppProjects/PO22/ty/POgui/readings/";
-str CSV_OUTPUT_PATH = "/home/pontiff/CppProjects/PO22/ty/POgui/results.csv";
+str PO_UTILITIES = "PO22utils/main.py";
+str CSV_READING_FOLDER = "readings/";
+str CSV_OUTPUT_PATH = "results.csv";
 #include "lib/dimensions.h"
 #define TYPE float
 #define FREQ 5
